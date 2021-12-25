64187d9cb13d1ee9695c241b0f5e97c84f766412
Pratik Pednekar <Pratik_Pednekar@McAfee.com>
2021-12-25 23:59:30 +0530

    return options

--- src/application/middleware/CorsMiddleware.ts
+++ src/application/middleware/CorsMiddleware.ts
@@ -8,6 +8,11 @@ export class CorsMiddleware {
         response.setHeader('Access-Control-Allow-Origin', '*');
         response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        if (request.method === "OPTIONS") {
+            return response.status(200).end();
+        }
+
         next();
 	};
 }
