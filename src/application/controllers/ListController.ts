e2fe114955f59854f13ea4de59474a5e88932cea
Pratik Pednekar <Pratik_Pednekar@McAfee.com>
2022-01-14 15:25:02 +0530

    .toString and return if list exists

--- src/application/controllers/ListController.ts
+++ src/application/controllers/ListController.ts
@@ -33,10 +33,10 @@ export class ListController {
             //return 409 conflict if it exists along with the Id.
             listId = await this.listValidator.checkIfListNameExists(createListDto.name, request.context.userId);
             if (listId)
-                response.status(409).send(listId.toString());
+               return response.status(409).send(listId.toString());
             
             listId = await this.createListUseCase.execute(createListDto, request.context.userId);
-            response.status(201).send(listId);
+            response.status(201).send(listId.toString());
         } catch (err: any) {
             this.logger.logError(err);
             response.send(500);
