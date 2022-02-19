5c0c5f5f46c2442f9628fedd390251be15ca67b4
Pratik Pednekar <Pratik_Pednekar@McAfee.com>
2022-02-19 12:04:31 +0530

    try puppeteer code

--- src/core/useCases/GetLinkPreviewUseCase.ts
+++ src/core/useCases/GetLinkPreviewUseCase.ts
@@ -4,6 +4,13 @@ const linkPreviewGenerator = require("link-preview-generator");
 import { Preview } from "../model/Preview";
 import { IGetLinkPreviewUseCase } from "../interfaces/useCases/IGetLinkPreviewUseCase";
 
+const puppeteer = require("puppeteer-extra");
+const pluginStealth = require("puppeteer-extra-plugin-stealth");
+const util = require("util");
+const request = util.promisify(require("request"));
+const getUrls = require("get-urls");
+const isBase64 = require("is-base64");
+
 
 @injectable()
 export class GetLinkPreviewUseCase implements IGetLinkPreviewUseCase {
@@ -17,8 +24,23 @@ export class GetLinkPreviewUseCase implements IGetLinkPreviewUseCase {
         let favicons: string[] = [];
  
         console.log('called link preview..')
-        const linkPreview = await linkPreviewGenerator(url, ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']);
 
+        console.log("setup puppeteer")
+        const browser = await puppeteer.launch(['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']);
+        const page = await browser.newPage();
+        page.setUserAgent("facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)");
+      
+        await page.goto(url);
+        await page.exposeFunction("request", request);
+        await page.exposeFunction("urlImageIsAccessible", true);
+
+        console.log("setup puppeteer completed")
+
+        console.log(await this.getImg(page, url));
+        await browser.close();
+        console.log('image retrieved.')
+        
+        const linkPreview = await linkPreviewGenerator(url, ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']);
         console.log(linkPreview);
 
         if ("title" in linkPreview)
@@ -38,4 +60,12 @@ export class GetLinkPreviewUseCase implements IGetLinkPreviewUseCase {
 
         return preview;
     }
+
+    getImg = async (page:any, uri:any) => {
+        const img = await page.evaluate(async () => {
+            const ogImg = document.querySelector('meta[property="og:image"]');
+            console.log(ogImg)
+        });
+        return img;
+    };
 }
